6efd90b4568c402bd0d583269f48f2ad6210a33f
turonov <turonov5400gmail.com>
2022-10-14 09:40:07 +0500

    2

--- build_func02.py
+++ build_func02.py
@@ -1,2 +1,8 @@
 # the task condition is placed in the README file
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func02
+number = 3
+number2 = 7/5
+number3 = 9/4
+
+answer = number*((number2)-(number3))
+print(answer)
